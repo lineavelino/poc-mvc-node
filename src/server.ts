cdf957e2dd83a6751a99fbde508d29da30c188a1
Aline Avelino <aline.savelino@gmail.com>
2022-06-27 17:24:00 -0300

    feat: creation post /users

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import { UserModel } from "./models/userModel";
 import { runDb } from "./services/connection";
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const user = new UserModel({
+    email,
+    name,
+    password
+  });
+
+  await user.save();
+
+  res.status(201).json({ data: user });
 });
 
 app.listen(port, () => {
--- src/services/connection.ts
+++ src/services/connection.ts
@@ -1,17 +1,6 @@
 import "dotenv/config";
 import { connect } from "mongoose";
-import { UserModel } from "../models/userModel";
 
 export async function runDb() {
   await connect(process.env.DB_URL!);
-
-  const user = new UserModel({
-    email: "aline@teste.com",
-    name: "Aline Avelino Rocha",
-    password: "pass123456"
-  });
-
-  await user.save();
-
-  console.log(user);
 }
